45887c9121e77505e569f7662022ea1dc07e4cb6
trungti98 <trungti09121998@gmai.com>
2021-04-07 17:39:29 +0700

    connect db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
-const express=require('express');
-const app=express();
-const PORT=5000
+const express = require('express');
+const mongoose = require('mongoose');
 
-app.get("/",(req,res)=>{
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://trungti98:trungti98@mern-learning.efanj.mongodb.net/mern-learning?retryWrites=true&w=majority`,
+            {
+                useCreateIndex: true,
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false
+            }
+        )
+        console.log('Mongoose connect')
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
+    }
+}
+
+connectDB();
+
+const app = express();
+const PORT = 5000
+
+app.get("/", (req, res) => {
     res.send("Hello world")
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server run ${PORT}`)
 })
